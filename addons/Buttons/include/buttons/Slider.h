adebbf2487892d29fd983bdcdc54fe7161786ba0
roxlu <diederick@apollomedia.nl>
2012-09-27 23:19:43 +0200

    Added decrement/increment buttons to a slider to fine tune values

--- addons/Buttons/include/buttons/Slider.h
+++ addons/Buttons/include/buttons/Slider.h
@@ -7,6 +7,13 @@
 
 namespace buttons {
 
+inline void set_slider_step_value(int& result) {
+	result = 1;
+}
+inline void set_slider_step_value(float& result) {
+	result = 0.01;
+}
+
 template<class T>
 class Slider : public Element {
 public:	
@@ -25,9 +32,25 @@ public:
 		,txtval_dx(-1)
 		,label_dx(-1)
 		,value_type(valueType)
-
+		,bt_dec_x(0)
+		,bt_inc_x(0)
+		,bt_y(0)
+		,bt_w(0)
+		,bt_h(0)
+		,bt_inc_top_color_ptr(NULL)
+		,bt_inc_bottom_color_ptr(NULL)
+		,bt_dec_top_color_ptr(NULL)
+		,bt_dec_bottom_color_ptr(NULL)
+		,stepv(0)
+		,bt_mouse_down(-1)
+		,bt_frames_down(0)
 	{
 		h = 22;
+		bt_inc_top_color_ptr = bt_top_col;
+		bt_inc_bottom_color_ptr = bt_bottom_col;
+		bt_dec_top_color_ptr = bt_top_col;
+		bt_dec_bottom_color_ptr = bt_bottom_col;
+		set_slider_step_value(stepv);
 	}
 	
 	~Slider() {
@@ -49,10 +72,14 @@ public:
 		bar_empty_color[3] = a;
 		bar_filled_color[3] = a;
 		bar_filled_bottom[3] = a;
-		
+		bt_top_col[3] = a;
+		bt_bottom_col[3] = a;
+
 		HSL_to_RGB(col_hue, col_sat, col_bright - 0.1, &bar_empty_color[0], &bar_empty_color[1], &bar_empty_color[2]);
 		HSL_to_RGB(col_hue, col_sat, col_bright + 0.4, &bar_filled_color[0], &bar_filled_color[1], &bar_filled_color[2]);
 		HSL_to_RGB(col_hue, col_sat, col_bright - 0.1, &bar_filled_bottom[0], &bar_filled_bottom[1], &bar_filled_bottom[2]);
+		HSL_to_RGB(col_hue, col_sat, col_bright - 0.1, bt_top_col, bt_top_col+1, bt_top_col+2);
+		HSL_to_RGB(col_hue, col_sat, col_bright - 0.2, bt_bottom_col, bt_bottom_col+1, bt_bottom_col+2);
 		return *this;
 	}
 
@@ -67,7 +94,7 @@ public:
 	
 	void generateDynamicText() {
 		txtval_dx = dynamic_text->add(x,y+2, "0.0", col_text[0], col_text[1], col_text[2], col_text[3]);
-		dynamic_text->setTextAlign(txtval_dx, TEXT_ALIGN_RIGHT, (w - 5));
+		dynamic_text->setTextAlign(txtval_dx, TEXT_ALIGN_RIGHT, (w - 50));
 	}
 	
 	void updateDynamicText() {
@@ -96,16 +123,78 @@ public:
 		int bar_x = x; // start x
 		int bar_y = (y + h) - bar_h; // start y
 
+		// button + / -
+		bt_w = 20;
+		bt_dec_x = bar_x + (w - bt_w * 2) - 1;
+		bt_inc_x = bar_x + (w - bt_w);
+		bt_y = y;
+		bt_h = h - bar_h;
+
+		buttons::createRect(vd, bt_dec_x, bt_y, bt_w, bt_h, bt_dec_top_color_ptr, bt_dec_bottom_color_ptr); // dec bg
+		buttons::createRect(vd, bt_inc_x, bt_y, bt_w, bt_h, bt_inc_top_color_ptr, bt_inc_bottom_color_ptr); // inc bg
+		buttons::createRect(vd, bt_inc_x + bt_w * 0.2, bt_y + bt_h * 0.45, bt_w * 0.6, 2, col_text, col_text); // + sign
+		buttons::createRect(vd, bt_inc_x + bt_w * 0.45, bt_y + bt_h * 0.20, 2, bt_h * 0.7, col_text, col_text); // + sign		
+		buttons::createRect(vd, bt_dec_x + bt_w * 0.2, bt_y + bt_h * 0.45, bt_w * 0.6, 2, col_text, col_text); // - sign
+
+		// slider
 		buttons::createRect(vd, bar_x, bar_y, bar_filled_w, bar_h, bar_filled_color, bar_filled_bottom);
 		buttons::createRect(vd, bar_x + bar_filled_w, bar_y, bar_empty_w, bar_h, bar_empty_color, bar_empty_color);
 	}
 
+	void update() {
+		// when you press the + or - buttons for several frames we auto inc/decr
+		if(bt_mouse_down == 0) {
+			++bt_frames_down;
+			if(bt_frames_down > 10) {
+				setValue(value + stepv);
+				bt_frames_down = 0;
+				needsRedraw();
+			}
+		}
+		else if(bt_mouse_down == 1) {
+			++bt_frames_down;
+			if(bt_frames_down > 10) {
+				setValue(value - stepv);
+				bt_frames_down = 0;
+				needsRedraw();
+			}
+		}
+	}
+
+	void onMouseDown(int mx, int my) {
+		// did we press one of the inc/dec buttons?
+		if(BMOUSE_INSIDE(mx,my,bt_inc_x, bt_y, bt_w, bt_h)) {
+			bt_mouse_down = 0;
+		}
+		else if(BMOUSE_INSIDE(mx,my,bt_dec_x, bt_y, bt_w, bt_h)) {
+			bt_mouse_down = 1;
+		}
+		else {
+			bt_mouse_down = -1;
+		}
+	}
+
 	void onMouseUp(int mx, int my) {
+
 		if(!is_mouse_inside) { // mouse up after
 			bg_top_color = col_bg_default;
 			bg_bottom_color = col_bg_default;
 			needsRedraw();
 		}
+		else {
+			// did we press one of the inc/dec buttons?
+			if(bt_mouse_down == 0 && BMOUSE_INSIDE(mx,my,bt_inc_x, bt_y, bt_w, bt_h)) {
+				setValue(value + stepv);
+				needsRedraw();
+			}
+			else if(bt_mouse_down == 1 && BMOUSE_INSIDE(mx,my,bt_dec_x, bt_y, bt_w, bt_h)) {
+				setValue(value - stepv);
+				needsRedraw();
+			}
+			//			bt_mouse_down = -1;
+		}
+		bt_frames_down = 0;
+		bt_mouse_down = -1;
 	}
 	
 	void onMouseEnter(int mx, int my) {
@@ -131,18 +220,53 @@ public:
 			needsTextUpdate();
 		}
 	}
+
+	void onMouseMoved(int mx, int my) {
+		float* curr_inc = bt_inc_top_color_ptr;
+		float* curr_dec = bt_dec_top_color_ptr;
+
+		// check increment button
+		if(BMOUSE_INSIDE(mx,my,bt_inc_x, bt_y, bt_w, bt_h)) {
+			bt_inc_top_color_ptr = bt_bottom_col;
+			bt_inc_bottom_color_ptr = bt_top_col;
+		}
+		else {
+			bt_inc_top_color_ptr = bt_top_col;
+			bt_inc_bottom_color_ptr = bt_bottom_col; //bt_bottom_col;
+		}
+
+		// check decrement button
+		if(BMOUSE_INSIDE(mx,my,bt_dec_x, bt_y, bt_w, bt_h)) {
+			bt_dec_top_color_ptr = bt_bottom_col;
+			bt_dec_bottom_color_ptr = bt_top_col;
+		}
+		else {
+			bt_dec_top_color_ptr = bt_top_col;
+			bt_dec_bottom_color_ptr = bt_bottom_col; //bt_bottom_col;
+		}
+
+		// when colors changes we redraw
+		if(bt_inc_top_color_ptr != curr_inc 
+			|| bt_dec_top_color_ptr != curr_dec) {
+			needsRedraw();
+		}
+	}
+
 	void setPercentage(const float perc) {
 		p = perc;
 		value = minv +((maxv-minv) * p);
 	}
 
 	void setValue(const T& v) {
-		value = v;
-		T absv = std::abs(minv + std::abs(v));
+		value = std::max<T>(minv, std::min<T>(maxv, v));
 		T nrange = maxv-minv;
 		p = (float)1.0f/nrange * (value-minv);
 	}
 	
+	Slider& setStep(const T s) {
+		stepv = s;
+	}
+
 	Slider& setMin(const T& m) {
 		minv = m; 
 		return *this;
@@ -191,6 +315,7 @@ public:
 	T& value;
 	T minv; // min value
 	T maxv; // max value 
+	T stepv; // step value
 	float p; // current percentage
 	int value_type; // SLIDER_FLOAT, SLIDER_INT (for storage)
 	
@@ -199,6 +324,21 @@ public:
 	float bar_empty_color[4];
 	int txtval_dx;
 	int label_dx;
+
+	// increment/decrement buttons
+	int bt_mouse_down; // 0 = inc, 1 = decr, -1 not in button
+	int bt_frames_down; // we count the number of frames the used pressed, after X-frames we auto increment
+	float bt_dec_x;
+	float bt_inc_x;
+	float bt_y;
+	float bt_w;
+	float bt_h; 
+	float bt_top_col[4];
+	float bt_bottom_col[4];
+	float* bt_inc_top_color_ptr;
+	float* bt_inc_bottom_color_ptr;
+	float* bt_dec_top_color_ptr;
+	float* bt_dec_bottom_color_ptr;
 };
 
 typedef Slider<float> Sliderf;
