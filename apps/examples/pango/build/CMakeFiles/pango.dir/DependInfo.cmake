676366584de69694ea866c0f5e5078918128ae2e
roxlu <info@roxlu.com>
2013-01-08 11:17:12 +0100

    Fixed line endings

---
+++ apps/examples/pango/build/CMakeFiles/pango.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/roxlu/roxlu/addons/Pango/src/pango/TextSurface.cpp" "/Users/roxlu/roxlu/apps/examples/pango/build/CMakeFiles/pango.dir/Users/roxlu/roxlu/addons/Pango/src/pango/TextSurface.cpp.o"
+  "/Users/roxlu/roxlu/apps/examples/pango/src/main.cpp" "/Users/roxlu/roxlu/apps/examples/pango/build/CMakeFiles/pango.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/roxlu/roxlu/lib/../lib/include"
+  "/Users/roxlu/roxlu/lib/../extern/include"
+  "/Users/roxlu/roxlu/addons/Pango/include"
+  "/Users/roxlu/roxlu/lib/../extern/include/pango-1.0"
+  "/Users/roxlu/roxlu/lib/../extern/include/glib-2.0"
+  "/Users/roxlu/roxlu/lib/../extern/include/cairo"
+  "/Users/roxlu/roxlu/lib/../extern/lib/mac/64/glib-2.0/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ apps/examples/pango/build/CMakeFiles/pango.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pango.dir/src/main.cpp.o"
+  "CMakeFiles/pango.dir/Users/roxlu/roxlu/addons/Pango/src/pango/TextSurface.cpp.o"
+  "pango.pdb"
+  "pango"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pango.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
