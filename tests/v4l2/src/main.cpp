1be74f6dc7fc67eceba1d4b7171769f39f3cf94d
root <root@gtx.(none)>
2013-01-10 21:56:42 +0100

    Added v4l2 test

---
+++ tests/v4l2/src/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <signal.h>
+#include "V4L2Capture.h"
+
+V4L2Capture cap;
+
+void sig_handler(int signum) {
+  printf("SIG HANDLER.\n");
+  cap.closeDevice();
+  exit(0);
+}
+
+int main() {
+  printf("Video4Linux2 Test\n");
+  
+  signal(SIGINT, sig_handler);
+
+
+  cap.setup();
+  cap.listDevices();
+  if(cap.openDevice(0)) {
+      cap.startCapture();
+      int num = 100000000;
+      for(int i = 0; i < num; ++i) {
+        //        printf("%d/%d\n", i, num);
+        cap.update();
+
+      }
+      cap.closeDevice();
+  }
+  return 0;
+}
