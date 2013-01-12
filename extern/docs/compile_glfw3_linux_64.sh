8250e75aab793a707b650cb9f15b0174b7d0469e
roxlu <info@roxlu.com>
2013-01-12 18:19:29 +0100

    Added revision for glfw

--- extern/docs/compile_glfw3_linux_64.sh
+++ extern/docs/compile_glfw3_linux_64.sh
@@ -8,6 +8,7 @@ fi
 
 if [ ! -d ${d}/glfw ] ; then 
     git clone https://github.com/elmindreda/glfw.git 
+    git reset --hard f0f4af42871aa8004747ce3df025e4cfef4d2d07
 fi
 
 if [ ! -d ${d}/compile ] ; then 
