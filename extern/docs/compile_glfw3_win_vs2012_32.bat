c10bdbe7d72b199ee88e901d376d50453a96c41e
Diederick Huijbers <diederick@apollomedia.nl>
2013-01-12 18:47:38 +0100

    GFLW was fixed for mac/linux!

--- extern/docs/compile_glfw3_win_vs2012_32.bat
+++ extern/docs/compile_glfw3_win_vs2012_32.bat
@@ -20,7 +20,7 @@ set d=%CD%
 
 if not exist %d%\glfw (
    git clone https://github.com/elmindreda/glfw.git 
-   git reset --hard f0f4af42871aa8004747ce3df025e4cfef4d2d07
+   git reset --hard 747b6d880563c0461df028d9e287652f16de7b17
 )
 
 call :compile_msvc compile_vs2010,build_vs2010,"Visual Studio 10"
