05652f421b57411d9633499784260dc6eba016ef
Diederick Huijbers <diederick@apollomedia.nl>
2013-01-21 22:27:36 +0100

    Added windows libraries + dlls

---
+++ apps/examples/pango/compile.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set d=%CD%
+
+if not exist "%d%\build" (
+   mkdir %d%\build
+)
+
+cd %d%\build
+cmake -G "Visual Studio 10" ..\
+cmake --build . --target install 
+:: -- /p:Configuration=Release /v:q
+:: %d%\bin\011_windows.exe
+:: cmake --build . --target install -- /p:Configuration=Debug
+
+:: msbuild Project.sln /v:m /p:useenv=true /p:Configuration=Release /t:rebuild /p:OutDir="../bin/"
+:: msbuild Project.sln /v:m /p:useenv=true /p:Configuration=Release /t:011_windows /p:OutDir="../bin/"
+msbuild Project.sln /v:m /p:useenv=true /p:Configuration=Release /p:OutDir="../bin/"
+
+cd %d%
