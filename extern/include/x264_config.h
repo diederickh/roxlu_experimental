845dc8235041d836436e87b72db75531858c28f2
Diederick Huijbers <diederick@apollomedia.nl>
2013-01-21 19:16:33 +0100

    Fixing windows

--- extern/include/x264_config.h
+++
@@ -1,8 +0,0 @@
-#define X264_BIT_DEPTH     8
-#define X264_GPL           1
-#define X264_INTERLACED    1
-#define X264_CHROMA_FORMAT 0
-#define X264_REV 2245
-#define X264_REV_DIFF 0
-#define X264_VERSION " r2245 bc13772"
-#define X264_POINTVER "0.129.2245 bc13772"
