554b19a66b0ece9e2233ea0fd8d6452c3b73ec02
roxlu <info@roxlu.com>
2013-02-04 12:17:54 +0100

    Renamed FLV screen recorder

---
+++ addons/FLV/include/flv/FLVScreenRecorder.h
@@ -0,0 +1,56 @@
+#ifndef ROXLU_FLV_SCREENRECORDER_H
+#define ROXLU_FLV_SCREENRECORDER_H
+
+#include <inttypes.h>
+#include <flv/AV.h>
+#include <flv/FLV.h>
+#include <roxlu/Roxlu.h>
+#include <string>
+
+#define SCREEN_RECORDER_USE_PBO
+#define SCREEN_RECORDER_NUM_PBOS 5
+
+struct FLVScreenRecorderSettings {
+	FLVScreenRecorderSettings();
+	~FLVScreenRecorderSettings();
+	void reset(); 
+
+	/* video settings */
+	unsigned int vid_in_w;
+	unsigned int vid_in_h;
+	unsigned int vid_out_w;
+	unsigned int vid_out_h;
+	int vid_fps;
+};
+
+class FLVScreenRecorder {
+ public:
+  FLVScreenRecorder();
+  ~FLVScreenRecorder();
+
+  bool setup(FLVScreenRecorderSettings settings);
+
+  bool open(std::string filepath);
+  
+  void grabFrame();
+
+  void start(); // must be called once, starts the encoding thread
+  void stop(); // shuts down the encoder, is automatically called by destructor too.
+
+ private:
+	FLVScreenRecorderSettings settings;
+
+  /* Recording */
+  AV av;
+  FLVFileWriter flv_writer;
+  FLV flv;
+  bool is_recording;
+  bool is_file_opened;
+
+  /* GL */
+  unsigned int channels;
+  unsigned char* pixels;
+  GLuint pbos[SCREEN_RECORDER_NUM_PBOS];
+  unsigned int dx;
+};
+#endif
---
+++ addons/FLV/src/flv/FLVScreenRecorder.cpp
@@ -0,0 +1,156 @@
+#include <flv/FLVScreenRecorder.h>
+
+// @todo we probably want a bigger distance between the read and write 
+// PBO indices.
+
+FLVScreenRecorder::FLVScreenRecorder() 
+  :channels(4)
+  ,pixels(NULL)
+  ,dx(0)
+  ,is_recording(false)
+  ,is_file_opened(false)
+{
+  memset(pbos, 0, sizeof(pbos));
+}
+
+FLVScreenRecorder::~FLVScreenRecorder() {
+#if !defined(SCREEN_RECORDER_USE_PBO) 
+  if(pixels != NULL) {
+    delete[] pixels;
+    pixels = NULL;
+  }
+#endif
+  is_file_opened = false;
+  is_recording = false;
+  channels = 0;
+  dx = 0;
+}
+
+bool FLVScreenRecorder::open(std::string filepath) {
+  RX_VERBOSE(("Open file: %s", filepath.c_str()));
+
+  if(!flv_writer.open(filepath)) {
+    return false;
+  }
+  is_file_opened = true;
+  return true;
+}
+
+bool FLVScreenRecorder::setup(FLVScreenRecorderSettings cfg) {
+	assert(cfg.vid_in_w != 0);
+	assert(cfg.vid_in_h != 0);
+	
+	settings = cfg;
+
+  av.setVerticalFlip(true);
+
+  bool r = av.setupVideo(settings.vid_in_w, settings.vid_in_h, 
+												 settings.vid_out_w, settings.vid_out_h, settings.vid_fps, 
+												 AV_FMT_BGRA32, &flv);
+
+	if(!r) {
+    return false;
+  }
+
+  flv.setCallbacks(&FLVFileWriter::write,
+                   &FLVFileWriter::rewrite,
+                   &FLVFileWriter::flush,
+                   &FLVFileWriter::close,
+                   &flv_writer);
+
+#if !defined(SCREEN_RECORDER_USE_PBO)
+  pixels = new unsigned char[settings.vid_in_w * settings.vid_in_h * channels];
+  memset(pixels, 0, settings.vid_in_w * settings.vid_in_h * channels);
+#endif
+
+  if(!av.initialize()) {
+    RX_WARNING(("cannot initialize AV."));
+    return false;
+  }
+
+#if defined(SCREEN_RECORDER_USE_PBO) 
+  glGenBuffers(SCREEN_RECORDER_NUM_PBOS, pbos);
+  for(int i = 0; i < SCREEN_RECORDER_NUM_PBOS; ++i) {
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, pbos[i]);
+    glBufferData(GL_PIXEL_PACK_BUFFER, settings.vid_in_w * settings.vid_in_h * channels, pixels, GL_STREAM_READ);
+  }
+
+  glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+#endif
+
+  return true;
+}
+
+void FLVScreenRecorder::grabFrame() {
+
+  if(!is_recording) {
+    RX_WARNING(("stopped, cant grab."));
+    return;
+  }
+
+#if !defined(SCREEN_RECORDER_USE_PBO)
+  if(av.wantsNewVideoFrame()) {
+    glReadPixels(0, 0, settings.vid_in_w, settings.vid_in_h, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+    av.addVideoFrame(pixels);
+  }
+#else
+  if(av.wantsNewVideoFrame()) {
+    RX_VERBOSE(("Grab a frame."));
+    rx_int64 now = rx_millis();
+    int write_dx = (dx++) % SCREEN_RECORDER_NUM_PBOS;
+    int read_dx = (write_dx + 1) % SCREEN_RECORDER_NUM_PBOS;
+
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, pbos[write_dx]);
+    glReadPixels(0, 0, settings.vid_in_w, settings.vid_in_h, GL_BGRA, GL_UNSIGNED_BYTE, 0); 
+
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, pbos[read_dx]);
+    GLubyte* ptr = (GLubyte*) glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
+    if(ptr) {
+      av.addVideoFrame(ptr);
+      glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+
+    }
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+  }
+
+#endif
+}
+
+void FLVScreenRecorder::start() {
+  if(is_recording) {
+    RX_WARNING(("already started."));
+    return;
+  }
+
+  RX_VERBOSE(("Start recording."));
+  is_recording = true;
+  av.start();
+}
+
+void FLVScreenRecorder::stop() {
+  if(!is_recording) {
+    RX_WARNING(("already stopped."));
+    return;
+  }
+
+  RX_VERBOSE(("Stop recording."));
+  av.stop();
+  is_recording = false;
+  is_file_opened = false; // av.stop() calls close on the flv writer
+}
+
+FLVScreenRecorderSettings::FLVScreenRecorderSettings() {
+	reset();
+}
+FLVScreenRecorderSettings::~FLVScreenRecorderSettings() {
+	reset();
+}
+
+void FLVScreenRecorderSettings::reset() {
+	vid_in_w = 0;
+	vid_in_h = 0;
+	vid_out_w = 0;
+	vid_out_h = 0;
+	vid_fps = 0;
+
+}
