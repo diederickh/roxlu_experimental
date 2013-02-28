d45b9d534b6f4fd82bd57eec990cff72d9dbec5b
roxlu <info@roxlu.com>
2013-02-28 22:29:44 +0100

    Tiny fixes onlinux

--- apps/examples/audio_recorder/build/cmake/compile.sh
+++ apps/examples/audio_recorder/build/cmake/compile.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-
+#set -x
 d=${PWD}
 bd=${d}/../../bin
 app=${PWD##*/}
 
-if [ ! -d ${b}/build ] ; then
+if [ ! -d ${d}/build ] ; then
    mkdir ${d}/build
 fi
 
-if [ ! -d ${bd} ] ; then 
+if [ ! -d ${bd}/data ] ; then 
    mkdir ${bd}
    mkdir ${bd}/data
 fi
 
 cd build
 cmake ../
+make VERBOSE=1
 make install
