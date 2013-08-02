83f347aaefdb485eb875f9b33f509b8538158b9b
roxlu <info@roxlu.com>
2013-08-02 10:20:36 +0200

    JPG write

--- addons/Image/include/image/JPG.h
+++ addons/Image/include/image/JPG.h
@@ -4,6 +4,14 @@
 #include <string>
 #include <stdio.h>
 
+#if defined(_WIN32)
+#  define XMD_H
+#  include <jpeglib.h>
+#  undef XMD_H
+#else 
+#  include <jpeglib.h>
+#endif
+
 #define ERR_JPG_FILE_NOT_OPENED "Cannot open the jpg file: `%s`"
 #define ERR_JPG_CANNOT_ALLOC "Cannot allocate buffer space for the jpg pixels "
 
@@ -20,6 +28,7 @@ class JPG {
   bool load(std::string filename, bool datapath = false);
   bool load(unsigned char* compressed, size_t nbytes);
   bool save(std::string filename, bool datapath = false);
+  bool setPixels(unsigned char* pix, int w, int h, J_COLOR_SPACE type = JCS_RGB);    /* JCS_RGB, JCS_GRAYSCALE, JCS_YCbCr, JCS_CMYK, JCS_Yckk */
   
   unsigned char* getPixels();
   unsigned int getWidth();
@@ -39,6 +48,12 @@ class JPG {
   unsigned int height;
   unsigned int bit_depth;
   unsigned int num_channels;
+  unsigned int quality;                                                              /* compression quality: 0 - 100, default is 80 */
+
+  /* libjpg types */
+  J_COLOR_SPACE color_space;
+  J_DCT_METHOD dct_method;
+  J_DITHER_MODE dither_mode; 
 };
 
 
